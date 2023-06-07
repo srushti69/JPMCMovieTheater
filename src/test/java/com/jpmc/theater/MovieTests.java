563b865c1a143a274fe4e70b0b4a4baddfa5396d
srushti69 <srushti_69@yahoo.com>
2023-06-07 11:39:20 -0400

    Fixed a test case.

--- src/test/java/com/jpmc/theater/MovieTests.java
+++ src/test/java/com/jpmc/theater/MovieTests.java
@@ -13,7 +13,7 @@ public class MovieTests {
     @Test
     void specialMovieWith50PercentDiscount() {
         Movie spiderMan = new Movie("Spider-Man: No Way Home", Duration.ofMinutes(90),12.5, 1);
-        Showing showing = new Showing(spiderMan, 5, LocalDateTime.of(LocalDate.now(), LocalTime.now()));
+        Showing showing = new Showing(spiderMan, 5, LocalDateTime.of(LocalDate.now(), LocalTime.of(10,50)));
         assertEquals(10, spiderMan.calculateTicketPrice(showing));
 
         System.out.println(Duration.ofMinutes(90));
